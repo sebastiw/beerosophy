4d05c91824f067407f12af6231767fb42f6aa896
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2017-09-10 23:14:26 +0200

    Add functional temperature measure script

---
+++ python/temp_measure.py
@@ -0,0 +1,41 @@
+import os
+import time
+import datetime
+sensor='28-03168c19a6ff' 
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = base_dir + sensor
+device_file = device_folder+'/w1_slave'
+
+def read_temp_raw(): #
+ f=open(device_file,'r')
+ lines=f.readlines()
+ f.close()
+ return lines
+
+def read_temp():
+ lines = read_temp_raw()
+ while lines[0].strip()[-3:]!='YES':
+  time.sleep(0.2)
+  lines = read_temp_raw()
+ equals_pos = lines[1].find('t=')
+ if equals_pos!=-1:
+  temp_string=lines[1][equals_pos+2:]
+  temp_c = float(temp_string)/1000.0
+  temp_f = temp_c * 9.0/5.0+32.0
+  return temp_c#,temp_f
+
+def main():
+ while True: #
+  temp = read_temp()
+  ts=time.time()
+  t=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+  print("Time:",t," Temperature:",temp,"degrees Celsius")
+  time.sleep(1) # seconds 
+
+
+if __name__=='__main__':
+ main()
